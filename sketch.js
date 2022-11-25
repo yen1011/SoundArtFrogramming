955934d455b4cc79d4475f3b6425e3b47b77c360
yen1011 <107763820+yen1011@users.noreply.github.com>
2022-11-25 17:27:15 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
 var wave;
-var playing = false;
 var button;
+var playing = false;
+
 function setup() {
   //createCanvas(displayWidth,displayHeight);
-createCanvas(640, 480);
+createCanvas(720, 256);
 wave = new p5.Oscillator();
 wave.setType('sine');
-wave.start();
-wave.freg(440);
-wave.amp(0);
+wave.freq(440);
+wave.amp();
 
-button = createButton('play/pause');
-button = mousePressed(toggle);
+button=createButton('play/pause');
+button.mousePressed(toggle);
+}
+
+function draw() {
+
 }
 
 function toggle() {
   if(!playing) {
+    wave.start();
     wave.amp(0.5, 1);
     playing = true;
-  } else {
+  } 
+  else {
     wave.amp(0,1);
     playing = false;
   }

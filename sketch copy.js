7a1974f369c0011d64be4b352ad0fa7eda933fec
yen1011 <107763820+yen1011@users.noreply.github.com>
2022-12-02 15:41:45 +0900

    Create sketch copy.js
    1202

---
+++ sketch copy.js
@@ -0,0 +1,20 @@
+let r, g, b;
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50, 255);
+}
+function draw() {
+  background(r, g, b);
+  console.log('draw');
+}
+
+function deviceMoved() {
+  r = map(accelerationX, -90, 90, 100, 175);
+  g = map(accelerationX, -90, 90, 100, 200);
+  b = map(accelerationX, -90, 90, 100, 200);
+}
+// function draw() {
+//   background(220);
+// }\watch
